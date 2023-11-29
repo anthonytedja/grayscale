96bfaca0ae6aad13abf83701ec022a57bf23b12f
tedjaant <anthony.tedja@mail.utoronto.ca>
2023-11-28 21:25:40 -0500

    Add README notebook & base files

--- src/config.py
+++ src/config.py
@@ -1,4 +1,12 @@
 import os
 
-# Training Hyperparameters
+DIR_ROOT = os.path.abspath('./')
+DIR_COLOR_IMAGES = os.path.join(DIR_ROOT, 'dataset/color/')
+DIR_GRAY_IMAGES = os.path.join(DIR_ROOT, 'dataset/gray/')
+
+# ------------------------------------------------------------ #
+
 IMAGE_SIZE = 224
+BATCH_SIZE = 10
+NUM_EPOCHS = 10
+GRADIENT_PENALTY_WEIGHT = 10
--- src/utils.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-import numpy as np
-import torch
-import torch.nn as nn
-import config
